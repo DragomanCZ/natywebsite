bd3fe82c197398f9783a9df0a3fa76c0bcbb6081
David Mrázek <91913988+DragomanCZ@users.noreply.github.com>
2023-11-13 00:37:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Assuming images are stored in the same directory as the HTML file
-    
-    var now = new Date();
+    // Get the current date
+    var today = new Date();
     var currentDateElement = document.getElementById("currentDate");
 
-    // Display the current date in a readable format (e.g., "November 12, 2023")
-    var options = { month: 'long', day: 'numeric', year: 'numeric' };
-    currentDateElement.textContent = now.toLocaleDateString('cs-CZ', options);
-    
-    today = new Date();
-    var cmas = new Date(2023, 10, 13); // Note: Months are zero-indexed, so December is 11
+    // Display the current date in Czech format (e.g., "12. listopadu 2023")
+    var options = { day: 'numeric', month: 'long', year: 'numeric', weekday: 'long' };
+    currentDateElement.textContent = today.toLocaleDateString('cs-CZ', options);
+
+    // Calculate the number of days remaining until December 13, 2023
+    var cmas = new Date(2024, 3, 18); // Note: Months are zero-indexed, so December is 11
     if (today.getMonth() == 11 && today.getDate() > 13) {
         cmas.setFullYear(cmas.getFullYear() + 1);
     }
     var one_day = 1000 * 60 * 60 * 24;
-    var number = Math.ceil((cmas.getTime() - today.getTime()) / one_day); // Using Math.ceil to ensure positive number
+    var number = Math.ceil((cmas.getTime() - today.getTime()) / one_day); // Using Math.ceil to ensure a positive number
     var imageUrl = number + ".jpeg"; // Concatenating with ".jpeg"
+    var textUrl = number + ".txt"; // Concatenating with ".txt"
 
+    // Set the image source
     var dynamicImage = document.getElementById("dynamicImage");
     dynamicImage.src = imageUrl;
- 
 
+    // Set the text file source
+    var dynamicText = document.getElementById("dynamicText");
+    dynamicText.data = textUrl;
+
+    // Fetch text content from the dynamically generated text file with UTF-16 encoding
+    fetch(textUrl, { headers: { 'Content-Type': 'text/plain; charset=utf-16' } })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('File not found');
+            }
+            return response.text();
+        })
+        .then(text => {
+            var textElement = document.createElement('p');
+            textElement.textContent = text;
+            document.querySelector('.container').appendChild(textElement);
+        })
+        .catch(error => {
+            console.error('Error fetching text:', error);
+            // Display "hi" if the file doesn't exist
+            var textElement = document.createElement('p');
+            textElement.textContent = "";
+            document.querySelector('.container').appendChild(textElement);
+        });
 });
